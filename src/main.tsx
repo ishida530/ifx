6ea020bac1f9b4cd28c788671abfe6196a2cbbce
ishida530 <ishida530@gmail.com>
2025-04-10 01:09:23 +0200

    setup core app structure with main entry and basic styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { router } from './routes/routes';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { persistQueryClient } from '@tanstack/query-persist-client-core';
+
+const queryClient = new QueryClient();
+
+persistQueryClient({
+  queryClient,
+  persister: createSyncStoragePersister({
+    storage: window.localStorage,
+  }),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
